0e81ac8a54aee1da1086c0775351b6a834a4409b
Jerlingit <jerlinthangam24@gmail.com>
2022-02-17 16:59:47 +0530

    VJ

--- src/test/java/org/tes/projects/Test.java
+++ src/test/java/org/tes/projects/Test.java
@@ -1,8 +1,10 @@
 package org.tes.projects;
 
 public class Test {
-
 	public static void test5() {
+		System.out.println("Tes5");
+	}
+	public static void test6() {
 		System.out.println("Tes5");
 	}
 }
